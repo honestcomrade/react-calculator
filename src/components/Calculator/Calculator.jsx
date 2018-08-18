2aaefe588f619024e832b535927e7307b0bddd8a
Joe Saraceno <honestcomrade@gmail.com>
2018-08-17 22:34:45 -0700

    works pretty well

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -9,39 +9,94 @@ class Calculator extends Component {
     this.state = {
       displayValue: '0',
       numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '.', 'ce'],
-      operators: ['/', 'x', '-', '+'],
+      operators: ['/', '*', '-', '+'],
       selectedOperator: '',
       storedValue: ''
     }
 
     this.updateDisplay = this.updateDisplay.bind(this);
+    this.setOperator = this.setOperator.bind(this);
+    this.callOperator = this.callOperator.bind(this);
   };
 
-  callOperator(props) {
-    console.log('Call operation - ', props);
-  }
-  setOperator(props) {
-    console.log('Set operation - ', props);
-  }
-  updateDisplay(value) {
-    let { displayValue } = this.state;
-    // prevent multiple '.'
-    if (value === '.' && displayValue.includes('.')) {
-      value = '';
+  callOperator() {
+    let {displayValue, selectedOperator, storedValue} = this.state;
+    // temp variable for updating state storedValue
+    const updateStoredValue = displayValue;
+
+    // parse strings for operations
+    displayValue = parseInt(displayValue, 10);
+    storedValue = parseInt(storedValue, 10);
+
+    // performs selected operation
+    switch(selectedOperator) {
+      case '+':
+        displayValue = storedValue + displayValue;
+        break;
+      case '-':
+        displayValue = storedValue - displayValue;
+        break;
+      case '*':
+        displayValue = storedValue * displayValue;
+        break;
+      case '/':
+        displayValue = storedValue / displayValue;
+        break;
+      default:
+        // set displayValue to zero if no case matches
+        displayValue = '0';
     }
 
-    if (value === 'ce') {
-      // delete last char from displayValue
-      displayValue = displayValue.substr(0, displayValue.length - 1);
-      // set displayValue to '0' if displayValue is emplty string
-      if (displayValue === '') {
-        displayValue = '0'
-      } 
+    // converts displayValue to a string
+    displayValue = displayValue.toString();
+    // reset selectedOperator
+    selectedOperator = '';
+    // check for 'NaN' or 'Infinity', if true set displayValue to '0'
+    if (displayValue === 'NaN' || displayValue === 'Infinity') displayValue ='0';
+
+    this.setState({displayValue, selectedOperator, storedValue: updateStoredValue});
+  };
+
+  setOperator(value) {
+    let {displayValue, selectedOperator, storedValue} = this.state;
+
+    // check if a value is already present for selectedOperator
+    if (selectedOperator === '') {
+      // update storedValue to the value of displayValue
+      storedValue = displayValue;
+      // reset the value of displayValue to '0'
+      displayValue = '0';
+      // update the value of selectedOperator to the given value
+      selectedOperator = value;  
     } else {
+      // if selectedOperator is not an empty string
+      // update the value of selectedOperator to the given value
+      selectedOperator = value;
+    }
+
+    this.setState({displayValue, selectedOperator, storedValue});
+  };
+
+  updateDisplay(value) {
+    let {displayValue} = this.state;
+
+    // prevent multiple occurences of '.'
+    if (value === '.' && displayValue.includes('.')) value = '';
+
+    if (value === 'ce') {
+      // deletes last char in displayValue
+      displayValue = displayValue.substr(0, displayValue.length - 1);
+      // set displayValue to '0' if displayValue is empty string
+      if (displayValue === '') displayValue = '0';
+    } else {
+      // replace displayValue with value if displayValue equal to '0'
+      // else concatenate displayValue and value
       displayValue === '0' ? displayValue = value : displayValue += value;
     }
-    this.setState({displayValue})
-  }
+
+    this.setState({displayValue});
+  };
+
   render() {
     const { displayValue, numbers, operators } = this.state;
     return (
